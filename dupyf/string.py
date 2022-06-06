0d8d62571b2bfbbafb6d38471539026fe1e3de64
Duduf <git@duduf.com>
2022-06-06 17:42:11 +0200

    removed unused string.py

--- dupyf/string.py
+++
@@ -1,7 +0,0 @@
-def get_baseName( filename ):
-    """Gets the name part of a filename without the extension"""
-    fileBaseName = filename
-    fileBaseNameList = filename.split('.')
-    if len(fileBaseNameList) > 1:
-        fileBaseName = '.'.join(fileBaseNameList[0:-1])
-    return fileBaseName
